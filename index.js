ba5fc9e4b33ebf74d2db3358ba16064b34654922
E W <exw@opmbx.org>
2017-07-11 20:56:38 +0000

    Fixed property desc bug

--- index.js
+++ index.js
@@ -15,8 +15,17 @@ var spawn = require('child_process').spawn;
 var json2csv = require('json2csv');
 var newLine = "\r\n";
 
+var pages = {};
+pages["under-construction"] = "http://www.bostonplans.org/projects/development-projects?projectstatus=under+construction&sortby=name&sortdirection=ASC&type=dev&viewall=1";
+pages["board-approved"] = "http://www.bostonplans.org/projects/development-projects?projectstatus=board+approved&sortby=name&sortdirection=ASC&type=dev&viewall=1"
+pages["under-review"] = "http://www.bostonplans.org/projects/development-projects?projectstatus=under+review&sortby=name&sortdirection=ASC&type=dev&viewall=1"
+
+console.log(process.argv);
+var page = pages[process.argv[2]];
+
 // var page = "https://bostonplans.org/projects/development-projects";
 // var page = "http://www.bostonplans.org/projects/development-projects?neighborhoodid=19&projectstatus=under+review&sortby=name&sortdirection=ASC&type=dev"
+
 // Under Review
 // var page = "http://www.bostonplans.org/projects/development-projects?projectstatus=under+review&sortby=name&sortdirection=ASC&type=dev&viewall=1"
 
@@ -24,8 +33,7 @@ var newLine = "\r\n";
 //var page = "http://www.bostonplans.org/projects/development-projects?projectstatus=board+approved&sortby=name&sortdirection=ASC&type=dev&viewall=1"
 
 //Under Construction
-
-var page = "http://www.bostonplans.org/projects/development-projects?projectstatus=under+construction&sortby=name&sortdirection=ASC&type=dev&viewall=1"
+//var page = "http://www.bostonplans.org/projects/development-projects?projectstatus=under+construction&sortby=name&sortdirection=ASC&type=dev&viewall=1"
 
 request(page, gotPage);
 
@@ -59,14 +67,13 @@ function projectPage(err, res, html) {
   // load csv information
   var dataList = $(".projectInfo li");
   var dataObj = {};
-  var dataFields = ["Property Description", "Address(es)", "Company Name(s)", "Proponents Name(s)","Address","Phone","Website Bio", "Notes", "Project URL"];
+  var dataFields = ["Property Description", "Address(es)", "Company Name(s)", "Proponents Name(s)","Address","Phone","Website Bio","Notes", "Project URL"];
   
   //populate temporary object
   var csvObj = {};
   for (i=0;i<dataFields.length;i++){
     csvObj[dataFields[i]] = "";
   }
-  var csvData = [];
   // populate objects, add to dataFields array if first time seen
   for (i=0;i<dataList.length;i++){
     var webFieldName = dataList[i].children[0].children[0].data;
@@ -78,28 +85,37 @@ function projectPage(err, res, html) {
     }*/
     switch(webFieldName) {
       case "Address:":
-        csvObj["Address(es)"] = webFieldData;
+        csvObj["Address(es)"] = "\"" + webFieldData + "\"";
         break;
-      case "Land Sq. Feet:":
-        csvObj["Property Description"] = webFieldData;
+      case "Building Size:":
+        csvObj["Property Description"] = "\"" + webFieldData + "\"";
         break;
       case "Residential Units:":
-        csvObj["Property Description"] += "," + webFieldData;
+        csvObj["Property Description"].replace(/\"\s*$/, "") += " with " + webFieldData + " res units\"";
+        break;
+      case "Uses:":
+        csvObj["Notes"] = webFieldData;
+        break;
+      default:
         break;
     }
       //console.log(res.request.uri.href);
-      
   } 
-  csvObj["URL:"] = res.request.uri.href; 
-  csvData.push(csvObj);
+  csvObj["Project URL"] = res.request.uri.href; 
 
   // console.log(dataObj);
 
   if (dataObj["Neighborhood:"] != undefined){
     var csvFileName = dataObj["Neighborhood:"].replace(/\s+/g, '-') + "-Projects.csv";
-    console.log(csvData);
-    var csvContent = json2csv({data: csvData, fields: dataFields}) + newLine;
-    console.log(csvData);
+    //console.log(csvData);
+    // var csvContent = json2csv({data: csvData, fields: dataFields}) + newLine;
+    var csvContent = "";
+    for (i=0;i<dataFields.length;i++){
+      csvContent += csvObj[dataFields[i]] + ",";
+    }
+    csvContent = csvContent.replace(/,\s*$/, "") + newLine;
+
+
     var downloadFolder = "./" + dataObj["Neighborhood:"].replace(/\s+/g, '-');
     mkdirIfReq(downloadFolder);
   }
@@ -115,14 +131,19 @@ function projectPage(err, res, html) {
     }
     else {
       console.log('Creating ' + csvFileName);
-      writeFields = (dataFields + newLine);
-      newFileContent = writeFields + csvContent;
-      fs.writeFile(csvFullPath, newFileContent, function (err, stat) {
-        if (err) console.log(err);
-        // console.log('Created ' + csvFileName);
-      });
+      var writeHeader = "";
+      var newFileContent = "";
+      for (i=0;i<dataFields.length;i++){
+        writeHeader += dataFields[i] + ",";
+        newFileContent = writeHeader.replace(/,\s*$/, "") + newLine + csvContent;
+        fs.writeFile(csvFullPath, newFileContent, function (err, stat) {
+          if (err) console.log(err);
+          // console.log('Created ' + csvFileName);
+          // 
+        });
+      }
     }
-  }); 
+  });
   // get documents on this page, if none visit documentsPage link
   var linkList = $(".documentLink");
   for (i=0;i<linkList.length;i++) {
@@ -187,10 +208,10 @@ function download_file_wget(file_url,file_name,folder){
   var filePath = folder + "/" + file_name;
   fs.stat(filePath, function(err, stat) {
     if (err == null) {
-      console.log("Skipping " + file_name + "Download, File Exists");
+      console.log("Skipping " + file_name + " Download, File Exists");
     }
     else {
-      console.log ("Download command issued for " + file_name + " at " + file_url);
+      console.log ("Downloading " + file_name + " at " + file_url);
       var wget = 'wget -O ' + folder + "/" + file_name + ' ' + file_url;
       var child = exec(wget, function(err, stdout, stderr) {
         if (err) console.log(err);
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p under-construction
+mkdir -p under-review
+mkdir -p board-approved
+cd under-construction
+node ../index.js under-construction
+cd ../under-review
+node ../index.js under-review
+cd ../board-approved
+node ../index.js board-approved
+
